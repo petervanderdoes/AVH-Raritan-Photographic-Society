7dd5a8a8416acaa4075ec1b7603c7ee8cafa397e
Peter van der Does <github@avirtualhome.com>
2012-08-08 15:44:27 -0400

    Prepare for work with git-flow

--- class/avh-rps.define.php
+++ class/avh-rps.define.php
@@ -6,7 +6,7 @@ final class AVH_RPS_Define
 	/**
 	 * General Constants
 	 */
-	const PLUGIN_VERSION = '1.0';
+	const PLUGIN_VERSION = '0.1.0';
 	const PLUGIN_README_URL = '';
 	const PLUGIN_FILE = 'avh-rps-competition/avh-rps-competition.php';
 	/**
