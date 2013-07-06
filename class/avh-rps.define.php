0325986be53b1d7e25c5075f58ae6ff3974ed4c9
Peter van der Does <peter@avirtualhome.com>
2013-07-05 20:30:22 -0400

    Preparation for new development cycle after release 1.1.1

--- class/avh-rps.define.php
+++ class/avh-rps.define.php
@@ -7,7 +7,7 @@ final class AVH_RPS_Define
 	/**
 	 * General Constants
 	 */
-	const PLUGIN_VERSION = '1.1.1';
+	const PLUGIN_VERSION = '1.2.0-dev.1';
 	const PLUGIN_README_URL = '';
 	const PLUGIN_FILE = 'avh-rps-competition/avh-rps-competition.php';
 	/**
