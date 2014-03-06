94e0ecc2e5a4dcdb6b6916583f638e8394affeca
Peter van der Does <peter@avirtualhome.com>
2014-03-06 15:49:43 -0500

    Version bump 1.3.9

--- app/RpsCompetition/Constants.php
+++ app/RpsCompetition/Constants.php
@@ -7,7 +7,7 @@ final class Constants
     /**
      * General Constants
      */
-    const PLUGIN_VERSION = '1.3.9-rc.1';
+    const PLUGIN_VERSION = '1.3.9';
 
     const PLUGIN_README_URL = '';
 
