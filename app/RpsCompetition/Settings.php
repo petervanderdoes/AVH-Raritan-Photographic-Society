0aeb4f265f1b7330634ac78bb363f0f139ea1ce7
Peter van der Does <peter@avirtualhome.com>
2014-06-19 19:40:33 -0400

    Use Settings class again.

---
+++ app/RpsCompetition/Settings.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pdoes
+ * Date: 6/19/14
+ * Time: 3:38 PM
+ */
+
+namespace RpsCompetition;
+
+use Avh\DataHandler\AttributeBagInterface;
+use Avh\DataHandler\DataHandler;
+
+class Settings extends DataHandler
+{
+    /**
+     * @param AttributeBagInterface $registry
+     */
+    public function __construct(AttributeBagInterface $registry)
+    {
+        parent::__construct($registry);
+    }
+}
