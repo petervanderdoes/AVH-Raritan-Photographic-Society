8e0a3832ea511606988f374074b25173676e2b90
Peter van der Does <peter@avirtualhome.com>
2014-07-08 10:13:07 -0400

    Remove unused use

--- app/RpsCompetition/Settings.php
+++ app/RpsCompetition/Settings.php
@@ -1,17 +1,9 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: pdoes
- * Date: 6/19/14
- * Time: 3:38 PM
- */
 
 namespace RpsCompetition;
 
-use Avh\DataHandler\AttributeBagInterface;
 use Avh\DataHandler\DataHandler;
 
 class Settings extends DataHandler
 {
-
 }
