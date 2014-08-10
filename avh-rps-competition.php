dcd0dcf42d17f77efec1f6a1e7703cd2bc2a56fb
Peter van der Does <peter@avirtualhome.com>
2014-08-10 15:24:57 -0400

    Version bump 1.4.6-rc.1

--- avh-rps-competition.php
+++ avh-rps-competition.php
@@ -3,7 +3,7 @@
  * Plugin Name: AVH RPS Competition
  * Plugin URI: http://blog.avirtualhome.com/wordpress-plugins
  * Description: This plugin was written to manage the competitions of the Raritan Photographic Society.
- * Version: 1.5.0-dev.1
+ * Version: 1.4.6-rc.1
  * Author: Peter van der Does
  * Author URI: http://blog.avirtualhome.com/
  * GitHub Plugin URI: https://github.com/petervanderdoes/AVH-Raritan-Photographic-Society
