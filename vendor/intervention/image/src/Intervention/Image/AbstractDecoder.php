76f26e4a78d5acff0cff5c689feec583e345cac1
Peter van der Does <peter@avirtualhome.com>
2014-09-17 14:56:57 -0400

    No fileinfo installed.

--- vendor/intervention/image/src/Intervention/Image/AbstractDecoder.php
+++ vendor/intervention/image/src/Intervention/Image/AbstractDecoder.php
@@ -138,6 +138,8 @@ abstract class AbstractDecoder
      */
     public function isBinary()
     {
+        return false;
+        
         if (is_string($this->data)) {
             $mime = finfo_buffer(finfo_open(FILEINFO_MIME_TYPE), $this->data);
             return (substr($mime, 0, 4) != 'text' && $mime != 'application/x-empty');
