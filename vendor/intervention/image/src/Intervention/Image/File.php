6e86fed127edd7097f9ce50343af86a9ef40e1f4
Peter van der Does <peter@avirtualhome.com>
2014-09-17 21:06:02 -0400

    finfo is not supported on our host.

--- vendor/intervention/image/src/Intervention/Image/File.php
+++ vendor/intervention/image/src/Intervention/Image/File.php
@@ -53,7 +53,8 @@ class File
         $this->filename = array_key_exists('filename', $info) ? $info['filename'] : null;
 
         if (file_exists($path) && is_file($path)) {
-            $this->mime = finfo_file(finfo_open(FILEINFO_MIME_TYPE), $path);
+            //$this->mime = finfo_file(finfo_open(FILEINFO_MIME_TYPE), $path);
+            $this->mime = 'image/jpeg';
         }
 
         return $this;
