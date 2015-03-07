edbe2f435d995081f784df36c4a76db2c7aca7b7
Peter van der Does <peter@avirtualhome.com>
2015-03-06 19:51:33 -0500

    Replace double quotes with single quotes

--- app/RpsCompetition/Db/QueryBanquet.php
+++ app/RpsCompetition/Db/QueryBanquet.php
@@ -39,12 +39,12 @@ class QueryBanquet
     public function getBanquets($season_start_date, $season_end_date)
     {
         $sql = $this->rpsdb->prepare(
-            "SELECT *
+            'SELECT *
 		FROM competitions
 		WHERE Competition_Date >= %s AND
 		  Competition_Date < %s AND
 		  Theme LIKE %s
-		ORDER BY ID",
+		ORDER BY ID',
             $season_start_date,
             $season_end_date,
             '%banquet%'
