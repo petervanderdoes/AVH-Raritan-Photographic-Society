cac27dce9b426ad9091669e9f8eaaf0d7d51f4d0
Peter van der Does <peter@avirtualhome.com>
2015-03-14 21:52:01 -0400

    Redundant check

--- app/RpsCompetition/Frontend/Requests/EditTitle/RequestEditTitle.php
+++ app/RpsCompetition/Frontend/Requests/EditTitle/RequestEditTitle.php
@@ -28,12 +28,12 @@ class RequestEditTitle
     /**
      * Constructor
      *
-     * @param EntityFormEditTitle $entity
-     * @param EditTitleType       $edit_title_type
+     * @param EntityFormEditTitle   $entity
+     * @param EditTitleType         $edit_title_type
      * @param RequestEditTitleModel $model
-     * @param IlluminateRequest   $request
-     * @param FormFactory         $form_factory
-     * @param Settings            $settings
+     * @param IlluminateRequest     $request
+     * @param FormFactory           $form_factory
+     * @param Settings              $settings
      *
      */
     public function __construct(
@@ -63,39 +63,35 @@ class RequestEditTitle
      */
     public function handleRequestEditTitle()
     {
-        global $post;
 
-        if (is_object($post) && $post->ID == 75) {
+        $form = $this->form_factory->create(
+            $this->edit_title_type,
+            $this->entity,
+            ['attr' => ['id' => 'edittitle']]
+        )
+        ;
+        $form->handleRequest($this->request);
 
-            $form = $this->form_factory->create(
-                $this->edit_title_type,
-                $this->entity,
-                ['attr' => ['id' => 'edittitle']]
-            )
-            ;
-            $form->handleRequest($this->request);
+        $redirect_to = $this->entity->getWpGetReferer();
 
-            $redirect_to = $this->entity->getWpGetReferer();
+        // Just return if user clicked Cancel
+        CommonHelper::isRequestCanceled($form, 'cancel', $redirect_to);
 
-            // Just return if user clicked Cancel
-            CommonHelper::isRequestCanceled($form, 'cancel', $redirect_to);
+        if (!$form->isValid()) {
+            $errors = $form->getErrors();
+            $this->settings->set('formerror', $errors);
 
-            if (!$form->isValid()) {
-                $errors = $form->getErrors();
-                $this->settings->set('formerror', $errors);
-
-                return;
-            }
-
-            $server_file_name = $this->entity->getServerFileName();
-            $new_title = $this->entity->getNewTitle();
-
-            if ($this->entity->getNewTitle() !== $this->entity->getTitle()) {
-                $this->model->updateTitle($server_file_name, $new_title);
-            }
-            $redirect_to = $this->entity->getWpGetReferer();
-            wp_redirect($redirect_to);
-            exit();
+            return;
         }
+
+        $server_file_name = $this->entity->getServerFileName();
+        $new_title = $this->entity->getNewTitle();
+
+        if ($this->entity->getNewTitle() !== $this->entity->getTitle()) {
+            $this->model->updateTitle($server_file_name, $new_title);
+        }
+        $redirect_to = $this->entity->getWpGetReferer();
+        wp_redirect($redirect_to);
+        exit();
     }
 }
