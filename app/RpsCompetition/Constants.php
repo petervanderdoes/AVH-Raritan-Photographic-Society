b6f74654be8685c9c0e3e85152695d23124042e8
Peter van der Does <peter@avirtualhome.com>
2015-03-17 14:46:56 -0400

    Version bump 2.0.8-rc.1

--- app/RpsCompetition/Constants.php
+++ app/RpsCompetition/Constants.php
@@ -39,7 +39,7 @@ final class Constants
     /**
      * General Constants
      */
-    const PLUGIN_VERSION = '2.0.8-dev.1';
+    const PLUGIN_VERSION = '2.0.8-rc.1';
     const SLUG_COMPETITION_EDIT = 'avh-rps-competition-edit';
     // @formatter:off
     private static $image_sizes = [
