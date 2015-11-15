914aa2d963d4283b872f5e4e8d619908748d8a66
Peter van der Does <peter@avirtualhome.com>
2015-11-15 14:26:04 -0500

    Clean up code

--- app/RpsCompetition/Api/Json.php
+++ app/RpsCompetition/Api/Json.php
@@ -11,53 +11,17 @@ namespace RpsCompetition\Api;
  */
 class Json
 {
-    const CONTENT_TYPE_DEBUG = 'application/json';
-    /**
-     * content type headers
-     */
     const CONTENT_TYPE_OFFICIAL = 'application/json';
-
-
-    /**
-     * json encode options
-     * default is JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE
-     */
-    const ENCODE_DEFAULT = JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE;
-    const ENCODE_DEBUG   = JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT;
-
-    /**
-     * advised http status codes
-     */
-    const STATUS_OK                    = 200;
-    const STATUS_PERMANENT_REDIRECT    = 308;
-    const STATUS_SERVICE_UNAVAILABLE   = 503;
-    const STATUS_TEMPORARY_REDIRECT    = 307;
-    const STATUS_UNAUTHORIZED          = 401;
-    const STATUS_UNPROCESSABLE_ENTITY  = 422;
-    const STATUS_BAD_REQUEST           = 400;
-    const STATUS_CREATED               = 201;
-    const STATUS_FORBIDDEN             = 403;
-    const STATUS_INTERNAL_SERVER_ERROR = 500;
-    const STATUS_METHOD_NOT_ALLOWED    = 405;
-    const STATUS_NOT_FOUND             = 404;
-    const STATUS_NOT_MODIFIED          = 304;
-    const STATUS_NO_CONTENT            = 204;
-
-    /**
-     * whether or not ->sendResponse() sends out basic status headers
-     * if set to true, it sends the status code and the location header
-     */
-    public static $send_status_headers = true;
-    protected     $http_status         = self::STATUS_OK;
-    protected     $included_resources  = [];
+    const JSON_ENCODE_DEFAULT   = JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE;
+    const JSON_STATUS_ERROR     = 'error';
+    const JSON_STATUS_FAIL      = 'fail';
+    const JSON_STATUS_SUCCESS   = 'success';
     /**
      * internal data containers
      */
-    protected $links             = [];
-    protected $meta_data         = [];
-    protected $redirect_location = null;
-    private   $data              = [];
-    private   $errors            = [];
+    private $data   = [];
+    private $errors = [];
+    private $status = null;
 
     public function __construct()
     {
@@ -86,66 +50,11 @@ class Json
     }
 
     /**
-     * Generate an array for the whole response body.
-     *
-     * @return array
+     * Remove all error messages.
      */
-    private function getArray()
+    public function cleanErrors()
     {
-        $response = [];
-        $response['data'] = $this->data;
-        if ($this->errors !== []) {
-            $response['errors'] = $this->errors;
-        }
-
-        return $response;
-    }
-
-    /**
-     * sends out the json response to the browser
-     * this will fetch the response from ->getJson() if not given via $response
-     *
-     * @note this also sets the needed http headers (status, location and content-type)
-     *
-     * @param  string $content_type   optional, defaults to ::CONTENT_TYPE_OFFICIAL (the official IANA registered one)
-     * @param  int    $encode_options optional, $options for json_encode()
-     *                                defaults to ::ENCODE_DEFAULT or ::ENCODE_DEBUG, @see ::$debug
-     * @param  json   $response       optional, defaults to ::getJson()
-     *
-     * @return void                   however, a string will be echo'd to the browser
-     */
-    public function sendResponse($content_type = null, $encode_options = null, $response = null)
-    {
-        if (is_null($response) && $this->http_status != self::STATUS_NO_CONTENT) {
-            $response = $this->getJson($encode_options);
-        }
-
-        if (empty($content_type)) {
-            $content_type = self::CONTENT_TYPE_OFFICIAL;
-        }
-
-        if (self::$send_status_headers) {
-            $this->sendStatusHeaders();
-        }
-
-        header('Content-Type: ' . $content_type . '; charset=utf-8');
-
-        if ($this->http_status == self::STATUS_NO_CONTENT) {
-            return;
-        }
-
-        echo $response;
-        die();
-    }
-
-    /**
-     * sets the http status code for this response
-     *
-     * @param int $http_status any will do, you can easily pass one of the predefined ones in ::STATUS_*
-     */
-    public function setHttpStatus($http_status)
-    {
-        $this->http_status = $http_status;
+        $this->errors = [];
     }
 
     /**
@@ -163,7 +72,7 @@ class Json
     public function getJson($encode_options = null)
     {
         if (is_int($encode_options) == false) {
-            $encode_options = self::ENCODE_DEFAULT;
+            $encode_options = self::JSON_ENCODE_DEFAULT;
         }
 
         $response = $this->getArray();
@@ -173,23 +82,84 @@ class Json
     }
 
     /**
-     * sends out the http status code and optional redirect location
-     * defaults to ::STATUS_OK, or ::STATUS_INTERNAL_SERVER_ERROR for an errors response
-     *
-     * @return void
+     * @return null
      */
-    private function sendStatusHeaders()
+    public function getStatus()
     {
-        if ($this->redirect_location) {
-            if ($this->http_status == self::STATUS_OK) {
-                $this->setHttpStatus(self::STATUS_TEMPORARY_REDIRECT);
-            }
+        return $this->status;
+    }
 
-            header('Location: ' . $this->redirect_location, true, $this->http_status);
+    /**
+     * @param null $status
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
 
-            return;
+    /**
+     * Send out the json response to the browser
+     *
+     * @param  string $content_type   optional, defaults to ::CONTENT_TYPE_OFFICIAL (the official IANA registered one)
+     * @param  int    $encode_options optional, $options for json_encode()
+     *                                defaults to ::JSON_ENCODE_DEFAULT
+     * @param  json   $response       optional, defaults to ::getJson()
+     *
+     * @return void                   however, a string will be echo'd to the browser
+     */
+    public function sendResponse($content_type = null, $encode_options = null, $response = null)
+    {
+        if (is_null($response)) {
+            $response = $this->getJson($encode_options);
         }
 
-        http_response_code($this->http_status);
+        if (empty($content_type)) {
+            $content_type = self::CONTENT_TYPE_OFFICIAL;
+        }
+
+        header('Content-Type: ' . $content_type . '; charset=utf-8');
+
+        echo $response;
+        die();
+    }
+
+    /**
+     * Generate an array for the whole response body.
+     *
+     * @return array
+     */
+    private function getArray()
+    {
+        $response = [];
+        if ($this->validateJsonStatus()) {
+            $response['status'] = $this->status;
+            if ($this->status !== self::JSON_STATUS_ERROR) {
+                $response['data'] = $this->data;
+            }
+            if ($this->errors !== []) {
+                $response['errors'] = $this->errors;
+            }
+        } else {
+            $response['status'] = self::JSON_STATUS_ERROR;
+            $this->cleanErrors();
+            $this->addError('Invalid status code given: ' . html_entity_decode($this->status));
+            $response['errors'] = $this->errors;
+        }
+
+        return $response;
+    }
+
+    /**
+     * Validate the JSON status code.
+     *
+     * @return bool
+     */
+    private function validateJsonStatus()
+    {
+        $valid_response[self::JSON_STATUS_ERROR] = true;
+        $valid_response[self::JSON_STATUS_ERROR] = true;
+        $valid_response[self::JSON_STATUS_ERROR] = true;
+
+        return array_key_exists($this->status, $valid_response);
     }
 }
