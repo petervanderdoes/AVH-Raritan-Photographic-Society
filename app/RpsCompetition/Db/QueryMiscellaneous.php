23f40b23c980aa2b8632d0a66e7890da08c71fce
Peter van der Does <peter@avirtualhome.com>
2016-03-22 21:32:08 -0400

    Reformat SQL statements

--- app/RpsCompetition/Db/QueryMiscellaneous.php
+++ app/RpsCompetition/Db/QueryMiscellaneous.php
@@ -58,15 +58,16 @@ class QueryMiscellaneous
         $competition_date_start = $this->rpsdb->getMysqldate($competition_date_start);
         $competition_date_end = $this->rpsdb->getMysqldate($competition_date_end);
 
-        $sql = $this->rpsdb->prepare('SELECT e.*
-            FROM competitions c, entries e
-                WHERE c.ID = e.Competition_ID and
-                    c.Competition_Date >= %s AND
-                    c.Competition_Date <= %s
-                ORDER BY RAND()',
+        $sql = $this->rpsdb->prepare("SELECT e.*
+FROM competitions c, entries e
+WHERE c.ID = e.Competition_ID AND
+      c.Competition_Date >= %s AND
+      c.Competition_Date <= %s
+ORDER BY RAND()",
                                      $competition_date_start,
                                      $competition_date_end);
         $result = $this->rpsdb->get_results($sql, ARRAY_A);
+        $return = [];
         foreach ($result as $record) {
             $e = new Entry();
             $e->map($record);
@@ -89,17 +90,23 @@ class QueryMiscellaneous
     {
         $competition_date_end = ($competition_date_end === null) ? $competition_date_start : $competition_date_end;
 
-        $sql = $this->rpsdb->prepare('SELECT c.Competition_Date, c.Medium, c.Classification, c.Special_Event,
-            if(c.Classification = "Beginner",0,
-            if(c.Classification = "Advanced",1,2)) as "Class_Code",
-            e.Score, e.Award, e.Member_ID
-            FROM competitions as c, entries as e
-            WHERE c.ID = e.Competition_ID AND
-                Competition_Date >= %s AND
-                Competition_Date < %s AND
-                Special_Event = "N" AND
-                c.Scored = "Y"
-            ORDER BY c.Medium DESC, Class_Code, c.Competition_Date',
+        $sql = $this->rpsdb->prepare("SELECT
+  c.Competition_Date,
+  c.Medium,
+  c.Classification,
+  c.Special_Event,
+  if(c.Classification = 'Beginner', 0,
+     if(c.Classification = 'Advanced', 1, 2)) AS 'Class_Code',
+  e.Score,
+  e.Award,
+  e.Member_ID
+FROM competitions AS c, entries AS e
+WHERE c.ID = e.Competition_ID AND
+      Competition_Date >= %s AND
+      Competition_Date < %s AND
+      Special_Event = 'N' AND
+      c.Scored = 'Y'
+ORDER BY c.Medium DESC, Class_Code, c.Competition_Date",
                                      $competition_date_start,
                                      $competition_date_end);
 
@@ -126,16 +133,22 @@ class QueryMiscellaneous
      */
     public function getEightsAndHigher($limit = 5)
     {
-        $sql = $this->rpsdb->prepare("SELECT c.Competition_Date, c.Classification,
-                if(c.Classification = 'Beginner',1,
-                if(c.Classification = 'Advanced',2,
-                if(c.Classification = 'Salon',3,0))) as \"Class_Code\",
-                c.Medium, e.Title, e.Server_File_Name, e.Award, e.Member_ID
-            FROM competitions c, entries e
-                WHERE c.ID = e.Competition_ID AND
-                    e.Score >= 8
-                ORDER BY RAND()
-                LIMIT %d",
+        $sql = $this->rpsdb->prepare("SELECT
+  c.Competition_Date,
+  c.Classification,
+  if(c.Classification = 'Beginner', 1,
+     if(c.Classification = 'Advanced', 2,
+        if(c.Classification = 'Salon', 3, 0))) AS \"Class_Code\",
+  c.Medium,
+  e.Title,
+  e.Server_File_Name,
+  e.Award,
+  e.Member_ID
+FROM competitions c, entries e
+WHERE c.ID = e.Competition_ID AND
+      e.Score >= 8
+ORDER BY RAND()
+LIMIT %d",
                                      $limit);
         $return = $this->rpsdb->get_results($sql);
 
@@ -151,16 +164,22 @@ class QueryMiscellaneous
      */
     public function getEightsAndHigherPerson($member_id)
     {
-        $sql = $this->rpsdb->prepare("SELECT c.Competition_Date, c.Classification,
-                if(c.Classification = 'Beginner',1,
-                if(c.Classification = 'Advanced',2,
-                if(c.Classification = 'Salon',3,0))) as \"Class_Code\",
-                c.Medium, e.Title, e.Server_File_Name, e.Award, e.Member_ID
-            FROM competitions c, entries e
-                WHERE c.ID = e.Competition_ID AND
-                    e.Member_ID = %s AND
-                    e.Score >= 8
-                ORDER BY c.Competition_Date, Class_Code, c.Medium, e.Score",
+        $sql = $this->rpsdb->prepare("SELECT
+  c.Competition_Date,
+  c.Classification,
+  if(c.Classification = 'Beginner', 1,
+     if(c.Classification = 'Advanced', 2,
+        if(c.Classification = 'Salon', 3, 0))) AS \"Class_Code\",
+  c.Medium,
+  e.Title,
+  e.Server_File_Name,
+  e.Award,
+  e.Member_ID
+FROM competitions c, entries e
+WHERE c.ID = e.Competition_ID AND
+      e.Member_ID = %s AND
+      e.Score >= 8
+ORDER BY c.Competition_Date, Class_Code, c.Medium, e.Score",
                                      $member_id);
         $return = $this->rpsdb->get_results($sql);
 
@@ -182,15 +201,20 @@ class QueryMiscellaneous
         $competition_date_start = $this->rpsdb->getMysqldate($competition_date_start);
         $competition_date_end = $this->rpsdb->getMysqldate($competition_date_end);
 
-        $sql = $this->rpsdb->prepare("SELECT MAX(z.Num_Awards) AS Max_Num_Awards FROM
-                (SELECT c.Competition_Date, c.Classification, c.Medium, COUNT(e.Award) AS Num_Awards
-                    FROM competitions c, entries e
-                        WHERE c.ID = e.Competition_ID AND
-                            c.Competition_Date >= %s AND
-                            c.Competition_Date <= %s AND
-                            Scored = 'Y' AND
-                            e.Award <> ''
-                        GROUP BY c.Competition_Date, c.Classification, c.Medium) z",
+        $sql = $this->rpsdb->prepare("SELECT MAX(z.Num_Awards) AS Max_Num_Awards
+FROM
+  (SELECT
+     c.Competition_Date,
+     c.Classification,
+     c.Medium,
+     COUNT(e.Award) AS Num_Awards
+   FROM competitions c, entries e
+   WHERE c.ID = e.Competition_ID AND
+         c.Competition_Date >= %s AND
+         c.Competition_Date <= %s AND
+         Scored = 'Y' AND
+         e.Award <> ''
+   GROUP BY c.Competition_Date, c.Classification, c.Medium) z",
                                      $competition_date_start,
                                      $competition_date_end);
         $return = $this->rpsdb->get_var($sql);
@@ -211,17 +235,26 @@ class QueryMiscellaneous
     {
         $competition_date_end = ($competition_date_end === null) ? $competition_date_start : $competition_date_end;
 
-        $sql = $this->rpsdb->prepare("SELECT c.ID as Competition_ID, c.Competition_Date, c.Classification,
-                if(c.Classification = 'Beginner',1,
-                if(c.Classification = 'Advanced',2,
-                if(c.Classification = 'Salon',3,0))) as \"Class_Code\", c.Medium, c.Theme, e.ID as Entry_ID, e.Title, e.Server_File_Name,
-                e.Score, e.Award
-            FROM competitions as c, entries as e
-            WHERE c.ID = e.Competition_ID AND
-                c.Competition_Date >= %s AND
-                c.Competition_Date <= %s AND
-                e.Member_ID = %s
-            ORDER BY c.Competition_Date, c.Medium",
+        $sql = $this->rpsdb->prepare("SELECT
+  c.ID                                         AS Competition_ID,
+  c.Competition_Date,
+  c.Classification,
+  if(c.Classification = 'Beginner', 1,
+     if(c.Classification = 'Advanced', 2,
+        if(c.Classification = 'Salon', 3, 0))) AS \"Class_Code\",
+  c.Medium,
+  c.Theme,
+  e.ID                                         AS Entry_ID,
+  e.Title,
+  e.Server_File_Name,
+  e.Score,
+  e.Award
+FROM competitions AS c, entries AS e
+WHERE c.ID = e.Competition_ID AND
+      c.Competition_Date >= %s AND
+      c.Competition_Date <= %s AND
+      e.Member_ID = %s
+ORDER BY c.Competition_Date, c.Medium",
                                      $competition_date_start,
                                      $competition_date_end,
                                      $user_id);
@@ -241,14 +274,18 @@ class QueryMiscellaneous
      */
     public function getSeasonList($order = 'ASC', $season_start_month_num, $season_end_month_num)
     {
-        $sql = $this->rpsdb->prepare('SELECT if(month(Competition_Date) >= %s and month(Competition_Date) <= %s,
-            concat_WS("-",year(Competition_Date),substr(year(Competition_Date)+1,3,2)),
-            concat_WS("-",year(Competition_Date)-1,substr(year(Competition_Date),3,2))) as "Season"
-            FROM competitions
-            GROUP BY Season
-            ORDER BY Season ' . $order,
-                                     $season_start_month_num,
-                                     $season_end_month_num);
+        $sql_statement = "SELECT if(month(Competition_Date) >= %s AND month(Competition_Date) <= %s,
+          concat_WS('-', year(Competition_Date), substr(year(Competition_Date) + 1, 3, 2)),
+          concat_WS('-', year(Competition_Date) - 1, substr(year(Competition_Date), 3, 2))) AS \"Season\"
+FROM competitions
+GROUP BY Season";
+        if (strtoupper($order) === 'ASC') {
+            $sql_statement .= 'ORDER BY Season ASC';
+        } else {
+            $sql_statement .= 'ORDER BY Season DESC';
+        }
+
+        $sql = $this->rpsdb->prepare($sql_statement, $season_start_month_num, $season_end_month_num);
 
         $result = $this->rpsdb->get_results($sql, ARRAY_A);
         $seasons = [];
@@ -269,15 +306,17 @@ class QueryMiscellaneous
      */
     public function getSeasonListWithEntries($season_start_month_num, $season_end_month_num)
     {
-        $sql = $this->rpsdb->prepare('SELECT if(month(c.Competition_Date) >= %s and month(c.Competition_Date) <= %s,
-            concat_WS(" - ",year(c.Competition_Date),substr(year(c.Competition_Date)+1,3,2)),
-            concat_WS(" - ",year(c.Competition_Date)-1,substr(year(c.Competition_Date),3,2))) as "Season",
-            count(e.ID)
-            FROM competitions c, entries e
-            WHERE c.ID = e.Competition_ID
-            GROUP BY Season
-            HAVING count(e.ID) > 0
-            ORDER BY Season',
+        $sql_statement = "SELECT
+  if(month(c.Competition_Date) >= %s AND month(c.Competition_Date) <= %s,
+     concat_WS(' - ', year(c.Competition_Date), substr(year(c.Competition_Date) + 1, 3, 2)),
+     concat_WS(' - ', year(c.Competition_Date) - 1, substr(year(c.Competition_Date), 3, 2))) AS \"Season\",
+  count(e.ID)
+FROM competitions c, entries e
+WHERE c.ID = e.Competition_ID
+GROUP BY Season
+HAVING count(e.ID) > 0
+ORDER BY Season";
+        $sql = $this->rpsdb->prepare($sql_statement,
                                      $season_start_month_num,
                                      $season_end_month_num);
 
@@ -301,17 +340,15 @@ class QueryMiscellaneous
      */
     public function getWinner($date, $award, $class)
     {
-        $sql = $this->rpsdb->prepare('SELECT e.* FROM entries e 
-                INNER JOIN competitions c 
-                ON (c.ID = e.Competition_ID AND
-                    c.Competition_Date =  %s AND
-                    e.Award = %s AND
-                    c.Classification = %s)
-                ORDER BY c.Medium',
+        $sql = $this->rpsdb->prepare("SELECT e.*
+FROM entries e INNER JOIN competitions c
+    ON (c.ID = e.Competition_ID AND c.Competition_Date = %s AND e.Award = %s AND c.Classification = %s)
+ORDER BY c.Medium",
                                      $date,
                                      $award,
                                      $class);
         $result = $this->rpsdb->get_results($sql, ARRAY_A);
+        $return = [];
         foreach ($result as $record) {
             $e = new Entry();
             $e->map($record);
@@ -333,26 +370,29 @@ class QueryMiscellaneous
     {
         $competition_date_end = ($competition_date_end === null) ? $competition_date_start : $competition_date_end;
 
-        $sql = $this->rpsdb->prepare('SELECT
-e.*,
-if(c.Classification = "Beginner",1, if(c.Classification = "Advanced",2, if(c.Classification = "Salon",3,0))) AS "Class_Code"
+        $sql_statement = "SELECT
+  e.*,
+  if(c.Classification = 'Beginner', 1,
+     if(c.Classification = 'Advanced', 2, if(c.Classification = 'Salon', 3, 0))) AS \"Class_Code\"
 FROM
-	entries e
-		INNER JOIN
-		competitions c
-		ON
-        (c.ID = e.Competition_ID AND
-         c.Competition_Date >= %s AND
-         c.Competition_Date <= %s AND
-         e.Award <> "")
+  entries e
+  INNER JOIN
+  competitions c
+    ON
+      (c.ID = e.Competition_ID AND
+       c.Competition_Date >= %s AND
+       c.Competition_Date <= %s AND
+       e.Award <> '')
 ORDER BY
-	c.Competition_Date,
-	Class_Code,
-	c.Medium,
-	e.Award',
+  c.Competition_Date,
+  Class_Code,
+  c.Medium,
+  e.Award";
+        $sql = $this->rpsdb->prepare($sql_statement,
                                      $competition_date_start,
                                      $competition_date_end);
         $result = $this->rpsdb->get_results($sql);
+        $return = [];
         foreach ($result as $record) {
             $e = new Entry();
             $e->map($record);
